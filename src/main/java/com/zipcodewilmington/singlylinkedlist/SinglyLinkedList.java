e377afb941d2f5de2081ab3b6d255b036a61b3fb
Danny Cao <dannycao1997@yahoo.com>
2024-03-25 21:20:58 -0400

    node head

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,18 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    private Node head;
+    private int size;
+
+    private class Node {
+        
+
+
+    }
+
 }
